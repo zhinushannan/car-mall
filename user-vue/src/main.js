3c38a34f0b809e59a512a7a463c5ed4ad6104b02
zhinushannan <1377875184@qq.com>
2021-11-28 16:37:29 +0800

    1、 新建 user-vue 项目，并完成基本配置

---
+++ user-vue/src/main.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+import $ from 'jquery'
+
+import axios from "axios";
+
+window.jQuery = $
+window.$ = $
+
+Vue.config.productionTip = false
+Vue.use(ElementUI);
+
+Vue.prototype.$axios = axios
+axios.defaults.baseURL="http://localhost:9000/user/"
+
+Vue.prototype.$layer = layer(Vue, {
+  msgtime: 3 //目前只有一项，即msg方法的默认消失时间，单位：秒
+});
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')

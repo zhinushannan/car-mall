9810a498837cacaf88072c602b9bafbb68af5377
zhinushannan <1377875184@qq.com>
2021-11-28 14:36:59 +0800

    1、 新建 admin-vue 前端项目，并搭建好管理员首页

---
+++ admin-vue/src/main.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+import layer from "vue-layer";
+import "vue-layer/lib/vue-layer.css";
+
+import $ from 'jquery'
+
+import axios from "axios";
+
+window.jQuery = $
+window.$ = $
+
+Vue.config.productionTip = false
+Vue.use(ElementUI);
+
+Vue.prototype.$axios = axios
+
+Vue.prototype.$layer = layer(Vue, {
+  msgtime: 3 //目前只有一项，即msg方法的默认消失时间，单位：秒
+});
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
